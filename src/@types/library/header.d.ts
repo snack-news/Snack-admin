9671889e4bb2d4197557f963ee806a9ebc9e66d3
martinyounghoonkim <hoons0131@gmail.com>
2019-08-25 22:33:14 +0900

    Add editorjs's header tool's interface

--- src/@types/library/header.d.ts
+++ src/@types/library/header.d.ts
@@ -1,7 +1,29 @@
 declare module '@editorjs/header' {
-  import { ToolSettings } from "@editorjs/editorjs";
-  import { ToolConstructable } from "@editorjs/editorjs";
+  import { SanitizerConfig, PasteEvent, API, BlockToolData, ToolConfig, BaseToolConstructable, BaseTool} from "@editorjs/editorjs";
+  import { ConversionConfig } from "@editorjs/editorjs/types/configs";
 
-  const k: ToolConstructable | ToolSettings;
+  interface HeaderTool extends BaseTool {
+    normalizeData (data: { text: string; level: number; }): { text: string; level: number; };
+    renderSettings(): HTMLElement;
+    setLevel(level: number): void;
+    merge(blockData: BlockToolData): void;
+    validate(blockData: BlockToolData): boolean;
+    save(block: HTMLElement): BlockToolData;
+    getTag(): HTMLElement;
+    onPaste(event: PasteEvent): void;
+    currentLevel: number;
+    defaultLevel: number;
+    levels: { number: number; tag: string; svg: HTMLOrSVGElement }[];
+    pasteConfig: { tags: string[] };
+    toolbox: { icon: string; title: 'Header' };
+    data: { text: string; level: number; };
+    sanitize: SanitizerConfig;
+    conversionConfig: ConversionConfig;
+  }
+  interface HeaderConstructor extends BaseToolConstructable {
+    new (config: {api: API, config: ToolConfig, data: BlockToolData}): HeaderTool;
+  }
+
+  const k: HeaderConstructor;
   export = k;
 }
