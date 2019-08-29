d7f9ef81a7388bf16f8d95c6d15c73a0f0a47372
martinyounghoonkim <hoons0131@gmail.com>
2019-08-29 22:19:04 +0900

    Add custom title component

---
+++ src/components/Utils/CustomTitle/CustomTitle.vue
@@ -0,0 +1,26 @@
+<template>
+  <component :is="tag" class="custom-title">
+    {{ title }}
+  </component>
+</template>
+<script lang="ts">
+  import { Component, Vue, Prop } from 'vue-property-decorator';
+
+  @Component
+  export default class CustomTitle extends Vue {
+    @Prop() tag!: string;
+    @Prop() title!: string;
+    mounted () {
+      console.log(this.tag);
+      console.log(this.title);
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  .custom-title {
+    margin: 0 0 48px 0;
+    font-size: 28px;
+    font-weight: bold;
+    color: #0f0f0f;
+  }
+</style>
