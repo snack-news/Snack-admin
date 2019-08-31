0aa129432595a4e160025842a9888e33537ef449
martinyounghoonkim <hoons0131@gmail.com>
2019-08-31 23:48:39 +0900

    Add custom button component

---
+++ src/components/Utils/CustomButton/CustomButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <button :class="[{ variant }, 'custom-button']" :type="type">
+    <slot />
+  </button>
+</template>
+<script lang="ts">
+  import { Component, Vue, Prop } from 'vue-property-decorator';
+
+  @Component
+  export default class CustomButton extends Vue {
+    @Prop({ default: "button" }) type!: string;
+    @Prop({ default: "primary" }) variant!: string;
+    @Prop({ default: "m" }) size!: string;
+
+    constructor () {
+      super();
+    }
+
+  }
+</script>
+<style lang="scss" scoped>
+
+</style>
