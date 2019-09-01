7c07968fdcd2c0edacf491a05824685f961cbf33
martinyounghoonkim <hoons0131@gmail.com>
2019-09-01 12:05:11 +0900

    Add size, color props in CustomButton component

--- src/components/Utils/CustomButton/CustomButton.vue
+++ src/components/Utils/CustomButton/CustomButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button :class="[{ variant }, 'custom-button']" :type="type">
+  <button :style="inlineStyle" :class="[`is-${color}`,`is-size-${size}`, 'custom-button']" :type="type">
     <slot />
   </button>
 </template>
@@ -9,8 +9,9 @@
   @Component
   export default class CustomButton extends Vue {
     @Prop({ default: "button" }) type!: string;
-    @Prop({ default: "primary" }) variant!: string;
+    @Prop({ default: "type01" }) color!: string;
     @Prop({ default: "m" }) size!: string;
+    @Prop() inlineStyle!: string;
 
     constructor () {
       super();
@@ -19,5 +20,29 @@
   }
 </script>
 <style lang="scss" scoped>
-
+  .custom-button {
+    /* 사이즈 관련 스타일 정의 */
+    &.is-size-m {
+      padding: 13px 31px 11px;
+      font-size: 18px;
+    }
+    &.is-size-l {
+      min-width: 201px;
+      padding: 20px 45px 18px;
+      font-size: 28px;
+    }
+    /* 스타일 관련 스타일 정의 */
+    &.is-type01 {
+      color: #fff;
+      background-color: #3559e4;
+    }
+    &.is-type02 {
+      color: #fff;
+      background-color: #0f0f0f;
+    }
+    &.is-type03 {
+      color: #fff;
+      background-color: #bbaeae;
+    }
+  }
 </style>
