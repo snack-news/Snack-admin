01bc4ad6d64abfb78cc11abb25b7f5bd6b087de5
martinyounghoonkim <hoons0131@gmail.com>
2019-09-04 23:19:47 +0900

    Add utility components
    - Date picker
    - Input Form's types

---
+++ src/components/Utils/DatePicker/DatePicker.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <vue-date-picker v-model="selectedDate"
+                     :lang="language" />
+  </div>
+</template>
+<script lang="ts">
+  import { Component, Vue, Watch, Prop } from "vue-property-decorator";
+  import VueDatePicker from "vue2-datepicker";
+  import Datepicker from "vue2-datepicker";
+  import format from "date-fns/format";
+
+  @Component({
+    components: {
+      VueDatePicker
+    }
+  })
+  export default class DatePicker extends Vue {
+    @Prop({ default: "yyyy-MM-dd" }) format!: string;
+
+    selectedDate: string;
+    language: Datepicker.Lang;
+
+    constructor() {
+      super();
+      this.selectedDate = "";
+      this.language = {
+        days: ["일", "월", "화", "수", "목", "금", "토"],
+        months: ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
+        pickers: ["next 7 days", "next 30 days", "previous 7 days", "previous 30 days"],
+        placeholder: {
+          date: "날짜를 선택해주세요.",
+          dateRange: "Select Date Range"
+        }
+      }
+    }
+
+    @Watch("selectedDate")
+    updateSelectedDate(value: Date) {
+      const formattedDate = format(value, "yyyy-MM-dd");
+      this.$emit("input", formattedDate);
+    }
+
+  }
+</script>
+<style lang="scss" scoped>
+</style>
--- src/components/Utils/InputForm/InputForm.vue
+++ src/components/Utils/InputForm/InputForm.vue
@@ -2,16 +2,31 @@
   <div :class="[{ 'has-paragraph': paragraph.length > 0 }, 'input-form']">
     <label>
       <strong>{{ label }}</strong>
-      <input :type="type"
+      <!-- 날짜 -->
+      <date-picker v-if="type === 'date'"
+                   v-model="localValue" />
+      <!-- // 날짜 -->
+      <select v-else-if="type === 'select'">
+        <option :key="option.value" v-for="option in options" :value="option.value">
+          {{ option.text }}
+        </option>
+      </select>
+      <!-- 인풋 -->
+      <input v-else
+             :type="type"
              v-model="localValue" />
+      <!-- // 인풋 -->
       <p v-if="paragraph.length > 0">{{ paragraph }}</p>
     </label>
   </div>
 </template>
 <script lang="ts">
-  import { Component, Vue, Prop, Watch } from 'vue-property-decorator';
+  import { Component, Vue, Prop, Watch } from "vue-property-decorator";
+  import DatePicker from "@/components/Utils/DatePicker/DatePicker.vue"
 
-  @Component
+  @Component({
+    components: { DatePicker }
+  })
   export default class InputForm extends Vue {
     localValue: string;
 
@@ -19,13 +34,15 @@
     @Prop({ default: "" }) value!: string;
     @Prop({ default: "" }) label!: string;
     @Prop({ default: "" }) paragraph!: string;
-    constructor () {
+    @Prop({ default: () => [] }) options!: { value: string; text: string; }[];
+
+    constructor() {
       super();
       this.localValue = this.value;
     }
 
     @Watch("localValue")
-    onChangeHandler (value: string) {
+    onChangeHandler(value: string) {
       this.$emit("input", value);
     }
 
@@ -38,10 +55,12 @@
     &.has-paragraph {
       margin-bottom: 24px;
     }
+
     label {
       position: relative;
       display: block;
       padding-left: 108px;
+
       strong {
         position: absolute;
         left: 0;
@@ -50,15 +69,35 @@
         font-weight: normal;
         color: #0f0f0f;
       }
-      input {
+
+      input, select {
         width: 100%;
+        height: 48px;
         padding: 12px 18px;
         border: solid 1px #0f0f0f;
         font-size: 15px;
         color: #0f0f0f;
+        -webkit-appearance: none;
+        -webkit-border-radius: 0px;
+        background: none;
       }
+
       p {
         margin-top: 12px;
+      }
+    }
+
+    /deep/ .mx-datepicker {
+      width: 100%;
+
+      input {
+        width: 100%;
+        height: 48px;
+        border-radius: 0;
+        padding: 12px 18px;
+        border: solid 1px #0f0f0f;
+        font-size: 15px;
+        color: #0f0f0f;
       }
     }
   }
