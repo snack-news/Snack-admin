d708d89e913b44f8861ad0de16ad6b15237b09e8
martinyounghoonkim <hoons0131@gmail.com>
2019-10-04 22:05:38 +0900

    Update news model's interface

--- src/@types/models/News.d.ts
+++ src/@types/models/News.d.ts
@@ -1,20 +1,40 @@
-interface INews {
+import { INullable } from "@/@types/utility";
+
+export interface IContent {
+  createAt: string;
+  modifiedAt: string;
+  id: number;
   title: string;
   content: string;
-  tags: ITag[];
-  link?: IExternalLink;
-  createdDate: number;
-  key: string;
+  link: INullable<string>;
+  category: {
+    id: number;
+    title: string;
+  };
+  topics: ITopic[];
+  tags: {
+    id: number;
+    title: string
+  }[];
+  publishAt: INullable<string>;
 }
 
-interface ITag {
-  highlight: boolean;
-  children: string;
-  key: string;
+export interface ITopic {
+  id: number;
+  type: string;
+  name: string;
+  image: null;
 }
 
-interface IExternalLink {
-  href: string;
-  title?: string;
-  img?: string;
+export interface IPageable {
+  offset: number;
+  pageNumber: number;
+  pageSize: number;
+  paged: boolean;
+  sort: {
+    empty: boolean;
+    sorted: boolean;
+    unsorted: boolean;
+  };
+  unpaged: boolean;
 }
